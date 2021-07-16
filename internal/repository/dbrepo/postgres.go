d82dda9e8a572727045df4f9ea880df007fdb42c
Yaroslav Vlasenko <yaroslavvlasenko@gmail.com>
2021-07-16 10:50:44 +0300

    SearchAvailabilityByDates

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -11,15 +11,17 @@ func (m *postgresDBRepo) AllUsers() bool {
 }
 
 // InsertReservation inserts reservation into the database
-func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
+func (m *postgresDBRepo) InsertReservation(res models.Reservation) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
+
+	var newId int
 
 	stmt := `insert into reservations (first_name, last_name, email, phone, start_date, 
 			end_date, room_id, created_at, updated_at) 
 			values ($1,$2,$3,$4,$5,$6,$7,$8,$9) returning id`
 
-	_, err := m.DB.ExecContext(ctx, stmt,
+	err := m.DB.QueryRowContext(ctx, stmt,
 		res.FirstName,
 		res.LastName,
 		res.Email,
@@ -29,11 +31,66 @@ func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
 		res.RoomID,
 		time.Now(),
 		time.Now(),
-	)
+	).Scan(&newId)
 
+	if err != nil {
+		return 0, err
+	}
+
+	return newId, nil
+}
+
+// InsertRoomRestriction inserts room_restrictions into the database
+func (m *postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `insert into room_restrictions (start_date, end_date, room_id, reservation_id,
+				 created_at, updated_at, restriction_id) 
+			values ($1,$2,$3,$4,$5,$6,$7)`
+
+	_, err := m.DB.ExecContext(ctx, stmt,
+		r.StartDate,
+		r.EndDate,
+		r.RoomID,
+		r.ReservationID,
+		time.Now(),
+		time.Now(),
+		r.RestrictionID,
+	)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+// SearchAvailabilityByDates returns true if availability exists for roomID, and false if no availability exists
+func (m *postgresDBRepo) SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	var numRows int
+
+	query := `
+			Select 
+				count(id) 
+			From 
+				room_restrictions 
+			Where 
+			    room_id = $1
+				$2 < end_date and $3 > start_date;
+			`
+
+	row := m.DB.QueryRowContext(ctx, query, roomID, start, end)
+	err := row.Scan(&numRows)
+	if err != nil {
+		return false, err
+	}
+
+	if numRows == 0 {
+		return true, nil
+	}
+
+	return false, nil
 }
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,11 +1,16 @@
 package repository
 
-import "github.com/yaroslavvlasenko/bookings/internal/models"
+import (
+	"github.com/yaroslavvlasenko/bookings/internal/models"
+	"time"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
-	InsertReservation(res models.Reservation) error
+	InsertReservation(res models.Reservation) (int, error)
+	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error)
 }
 
 
